10a0a6f085fcc5c97731538cf22e1d279ef0787c
majdan <majdanski.dawid@gmail.com>
2015-05-23 21:40:49 +0200

    Majdan/Konwersja: gettery/settery i test dla metody

--- spec/Majdan/Tools/KonwersjaSpec.php
+++ spec/Majdan/Tools/KonwersjaSpec.php
@@ -11,4 +11,16 @@ class KonwersjaSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Majdan\Tools\Konwersja');
     }
+
+    function it_should_have_setter_and_gettter()
+    {
+        $this->setDana(123)->getDana()->shouldReturn(123);
+        $this->setJednostka1("lorem")->getJednostka1()->shouldReturn("lorem");    
+        $this->setJednostka2("lore")->getJednostka2()->shouldReturn("lore");  
+    }
+
+    function it_should_test()
+	{
+  	  $this->setDana(5)->test()->shouldReturn(5);
+	}
 }
--- src/Majdan/Tools/Konwersja.php
+++ src/Majdan/Tools/Konwersja.php
@@ -4,4 +4,50 @@ namespace Majdan\Tools;
 
 class Konwersja
 {
+
+    private $dana;
+    private $jednostka1;
+    private $jednostka2;
+
+    public function setDana($dana)
+    {
+        $this->dana = $dana;
+
+        return $this;
+    }
+
+	public function getDana()
+    {
+        return $this->dana;
+    }
+
+   public function setJednostka1($jednostka1)
+    {
+        $this->jednostka1 = $jednostka1;
+
+        return $this;
+    }
+
+	public function getJednostka1()
+    {
+        return $this->jednostka1;
+    }
+
+    public function setJednostka2($jednostka2)
+    {
+        $this->jednostka2 = $jednostka2;
+
+        return $this;
+    }
+
+	public function getJednostka2()
+    {
+        return $this->jednostka2;
+    }
+
+
+    public function test()
+    {
+        return $this->dana;
+    }
 }
